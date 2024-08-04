3f22af27adce038764ef39a4315a4b4725c86a7c
Tonie Victor <victornwanochi@gmail.com>
2024-08-03 20:11:07 -0700

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+	constructor() {
+		this.client = createClient();
+		this.client.on('error', (error) => {
+			console.log(`Redis client not connected to server: ${error}`);
+		});
+	}
+
+	isAlive() {
+		return this.isClientConnected;
+	}
+
+	async get(key) {
+		return promisify(this.client.GET).bind(this.client)(key);
+	}
+	
+	async set(key, value, duration) {
+		await promisify(this.client.SETEX)
+			.bind(this.client)(key, duration, value);
+	}
+
+	async del(key) {
+		await promisify(this.client.DEL).bind(this.client)(key);
+	}
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
