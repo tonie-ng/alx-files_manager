16203ca815ab2f9fea3d47e488f00db657cf1f54
Tonie Victor <victornwanochi@gmail.com>
2024-08-04 15:17:12 -0700

    linting

--- utils/redis.js
+++ utils/redis.js
@@ -2,29 +2,29 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-	constructor() {
-		this.client = createClient();
-		this.client.on('error', (error) => {
-			console.log(`Redis client not connected to server: ${error}`);
-		});
-	}
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
 
-	isAlive() {
-		return this.client.connected;
-	}
+  isAlive() {
+    return this.client.connected;
+  }
 
-	async get(key) {
-		return promisify(this.client.GET).bind(this.client)(key);
-	}
-	
-	async set(key, value, duration) {
-		await promisify(this.client.SETEX)
-			.bind(this.client)(key, duration, value);
-	}
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
 
-	async del(key) {
-		await promisify(this.client.DEL).bind(this.client)(key);
-	}
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
 
 export const redisClient = new RedisClient();
