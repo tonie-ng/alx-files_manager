f30d87dbcdf04500d4cbd3a77f70b91e3514a90c
Tonie Victor <victornwanochi@gmail.com>
2024-08-07 18:57:46 -0700

    First file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
@@ -16,5 +17,17 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
