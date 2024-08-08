38dcd24daa237565767e27a3b9929fe0754e996e
Tonie Victor <victornwanochi@gmail.com>
2024-08-07 18:15:26 -0700

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
 
 const router = Router();
@@ -6,5 +7,7 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
