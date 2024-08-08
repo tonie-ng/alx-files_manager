c7a2aaf2f0d012db7b8d9bfcbe024366a6af7d6c
Tonie Victor <victornwanochi@gmail.com>
2024-08-07 18:18:28 -0700

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
