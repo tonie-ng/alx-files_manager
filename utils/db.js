ccfcc206570abf9663ec54539a404773aaabc386
Tonie Victor <victornwanochi@gmail.com>
2024-08-07 17:22:28 -0700

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.DB_PORT || 27017;
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${HOST}:${PORT}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.db.collection('users');
+    return users.countDocuments();
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    return files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
